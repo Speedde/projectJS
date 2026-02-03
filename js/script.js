f0d0a1958c013477daa62925d3312ce4f42189be
Bohdan Sokol <bohdasokol@ukr.net>
2026-02-04 00:19:45 +0200

    First lesson

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 "use strict"
-alert("Hello!");
+const numberOfFilms = +prompt('Скільки фільмів ви подивилися?', "");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+for(let i = 0; i < 2; i++) {
+    let filmName = prompt("Один з останніх переглянутих фільмів", ''),
+    filmsScore = +prompt("Наскільки оцінюєте його?", "");
+    personalMovieDB.movies[`${filmName}`] = filmsScore;
+}
+
+console.log(personalMovieDB);
+
