667173c966045dd8e5dd41e642d83705417b97c4
Bohdan Sokol <bohdasokol@ukr.net>
2026-02-06 01:17:44 +0200

    Third day + fibonaccy

--- js/script.js
+++ js/script.js
@@ -3,7 +3,14 @@
     'use strict';
 }());
 
-const numberOfFilms = +prompt('Скільки фільмів ви подивилися?', "");
+let numberOfFilms;
+function start() {
+    numberOfFilms = +prompt('Скільки фільмів ви подивилися?', "");
+    while(!numberOfFilms) {
+        start();
+    }
+}
+// start();
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -11,31 +18,48 @@ const personalMovieDB = {
     genres: [],
     private: false
 };
-
-for(let i = 0; i < 2; i++) {
-    let filmName = prompt("Один з останніх переглянутих фільмів", '');
-    let filmsScore = +prompt("Наскільки оцінюєте його?", "");
-    while(!filmName || !filmsScore || isNaN(filmsScore) || filmName.length > 50 || filmsScore > 11) {
-        filmName = prompt("Один з останніх переглянутих фільмів", '');
-        filmsScore = +prompt("Наскільки оцінюєте його?", "");
+function askMeAboutFilm() {
+    for(let i = 0; i < 2; i++) {
+        let filmName = prompt("Один з останніх переглянутих фільмів", '');
+        let filmsScore = +prompt("Наскільки оцінюєте його?", "");
+        while(!filmName || !filmsScore || isNaN(filmsScore) || filmName.length > 50 || filmsScore > 11) {
+            filmName = prompt("Один з останніх переглянутих фільмів", '');
+            filmsScore = +prompt("Наскільки оцінюєте його?", "");
+        }
+        
+        personalMovieDB.movies[`${filmName}`] = filmsScore;
     }
-    
-    personalMovieDB.movies[`${filmName}`] = filmsScore;
 }
-if (personalMovieDB.count) {
-    if (personalMovieDB.count < 10) {
-        alert('Побачено так мало фільмів');
-    } else if (personalMovieDB.count <= 30) {
-        alert('Ви класичний глядач');
-    } else if (personalMovieDB.count > 30) {
-        alert('Ви кіноман'); 
-    } 
-} else {
-    alert("Виникла помилка");
+// askMeAboutFilm();
+function rememberFilm() {
+    if (personalMovieDB.count) {
+        if (personalMovieDB.count < 10) {
+            alert('Побачено так мало фільмів');
+        } else if (personalMovieDB.count <= 30) {
+            alert('Ви класичний глядач');
+        } else if (personalMovieDB.count > 30) {
+            alert('Ви кіноман'); 
+        } 
+    } else {
+        alert("Виникла помилка");
+    }
 }
+// rememberFilm();
+function showDB (hidden) {
+    if(!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+// showDB(personalMovieDB.private);
+function rememberMyGenre () {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt('Якому жанру вивіддаєте перевагу під номером ' + i);
+    }
+}
+// rememberMyGenre();
 
 
-console.log(personalMovieDB);
+
 
  
 // let result = '';
@@ -106,3 +130,115 @@ console.log(personalMovieDB);
 //     result += "\n";
 // }
 // console.log(result);
+
+// function getMathResult(a, b) {
+//     let str = "";
+//     let div = "---";
+//     if(typeof(a) === 'number' && typeof(b) === 'number' && b > 0 && !isNaN(b)) {
+//         for (let i = 1; i <= b; i++) {
+//             str += a * i;
+//             if(i == b) continue;
+//             str += div;
+//         }
+//         return str;
+//     } else {
+//         return a;
+//     }
+// }
+// console.log(getMathResult(20, -5));
+
+// function calculateVolumeAndArea(a) {
+//     let str = '';
+//     if(typeof(a) === 'number' && Number.isInteger(a) && a > 0) {
+//         str += 'Объем куба: ' + Math.pow(a, 3) + `, площадь всей поверхности: ${Math.pow(a, 2) * 6} `;
+//         return str;
+//     } else {
+//         str += 'При вычислении произошла ошибка';
+//         return str;
+//     }
+// }
+// let b = +prompt('length of one side', '');
+// console.log(calculateVolumeAndArea(b));
+
+// function getCoupeNumber(a) {
+//     let str = '';
+    
+//     if(isNaN(a) || !Number.isInteger(a) || a < 0) {
+//         str += 'Ошибка. Проверьте правильность введенного номера места';
+//     } else if(a > 36 || a == 0) {
+//         str += "Таких мест в вагоне не существует";
+//     } else {
+//         for (let i = 1; i <= 9; i++) {
+//             if((a / 4) <= i) {
+//                 str += i;
+//                 break;
+//             }
+//         }
+//     }
+//     return str;
+    
+// }
+// console.log(getCoupeNumber(17));
+// function getTimeFromMinutes (a) {
+//     if(isNaN(a) || !Number.isInteger(a) || a < 0 || a > 600) {
+//         return "Ошибка, проверьте данные";
+//     }
+//     const h = Math.floor(a / 60);
+//     let str = h;
+//     switch(h) {
+//         case 0: {str += " годин"; break;}
+//         case 1: {str += " годинa"; break;}
+//         case 2: {str += " години"; break;}
+//         case 3: {str += " години"; break;}
+//         case 4: {str += " години"; break;}
+//         case 5: {str += " годин"; break;}
+//         case 6: {str += " годин"; break;}
+//         case 7: {str += " годин"; break;}
+//         case 8: {str += " годин"; break;}
+//         case 9: {str += " годин"; break;}
+//         case 10: {str += " годинa"; break;}
+        
+//     }
+//     return str;
+// }
+// console.log(getTimeFromMinutes(150));
+// function findMaxNumber(a, b, c, d) {
+//     if(isNaN(a) || isNaN(b) || isNaN(c) || isNaN(d) || !a || !b || !c || !d) {
+//         return 0;
+//     }
+//     if(a > b) {
+//         if(a > c) {
+//             if(a > d) {
+//                 return a;
+//             } else return d;
+//         } else if(c > d) {return c;
+//         } else return d;
+//     } else if(b > c) {
+//         if(b > d) {
+//             return b;
+//         } else return d;
+//     } else if(c > d) {
+//         return c;
+//     } else return d;
+// }
+// console.log(findMaxNumber(103, 200, 4.5, 4));
+function fib(a) {
+    let str ='';
+    if (typeof(a) !== 'number' || !Number.isInteger(a) || a < 0) {
+        return str;
+    }
+    let n1 = 0, n2 = 1;
+    for (let i = 0; i < a; i++) {  
+        if((i + 1) === a) {
+            str += n1;
+        } else {
+            str += n1 + " ";
+        }                        
+        let n3 = n1 + n2;
+        n1 = n2;
+        n2 = n3;
+        
+    }
+    return str;
+}
+console.log(fib(7));
